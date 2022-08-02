116be7d8741d3f8c961f3145ab3aac0ff31fdbe6
Tsering Tsering <dtsering2@gmail.com>
2022-08-02 12:43:55 -0400

    embeded playlist

--- index.js
+++ index.js
@@ -87,14 +87,11 @@ const formContainer = document.querySelector('div.formContainer')
             //sumbit-> boolean if pass & confirm pass match => true
                             //post new user into index.json
                             //send a new alert "Welcome to myPOMODORO!"
-                            //after 1.5 sec render page without form
                   //-> boolean if pass & confirm pass no match => false
                             //send a alert "Password doesn't match"
                             //reset form 
 
-
-
-        //want to access
+        //cant access this element unless its loaded after button clicked
         document.addEventListener("DOMContentLoaded", ()=>{
             //event listener to exit log in form
             loginCancel.addEventListener('click',()=>{
@@ -104,3 +101,23 @@ const formContainer = document.querySelector('div.formContainer')
             });
         });
     })
+
+//Random quote generator
+async function getAllQuotes() {
+    let res = await fetch('https://type.fit/api/quotes');
+    let data = await res.json() ;
+    allQuotes = data;
+    renderOneQuote(data)
+}
+
+function renderOneQuote() {
+    let randomQuote = allQuotes[Math.floor(Math.random() * allQuotes.length)];
+    console.log(randomQuote);
+    let textOfQuote = document.querySelector('#quoteText');
+    let authorOfQuote = document.querySelector('#quoteAuthor');
+    textOfQuote.innerText = `"${randomQuote.text}"`;
+    if (randomQuote.author != null ) {authorOfQuote.innerText = ` - ${randomQuote.author}`}
+    else {authorOfQuote.innerText = ``};
+    setTimeout(renderOneQuote, 120000);
+}
+getAllQuotes()
