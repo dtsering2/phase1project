7923b2fb3db9fa98f96ea16742453101a58d0154
Tsering Tsering <dtsering2@gmail.com>
2022-08-02 01:10:31 -0400

    basic event listener to open/close form, still missing few html

---
+++ index.js
@@ -0,0 +1,106 @@
+//elements for settings button/login button
+const formContainer = document.querySelector('div.formContainer')
+    //specific for setting
+    const settingContainer = document.querySelector('div.settingContainer')
+    const settingsbtn = document.querySelector("button#settings")
+    const settingCancel = document.querySelector('button.settingCancelBtn')
+    const settingFormSubmit= document.querySelector('form#settingForm')
+    //specific for login
+    const loginContainer = document.querySelector('div.loginContainer')
+    const loginbtn = document.querySelector('button#login')
+    const loginCancel = document.querySelector("button.loginCancelBtn")
+    const loginFormSubmit = document.querySelector('form#loginForm')
+    const signupbtn = document.querySelector('button#signUp')
+    //specific for signup
+    const signupContainer = document.querySelector('div.signupContainer')
+    const timerparentContainer = document.querySelector('div.timerParentContainer')
+    const signupCancel = document.querySelector('div.signinCancelBtn')
+
+//Event listeners for setting button
+    //mouse over event to change text color
+    settingsbtn.addEventListener('mouseover', ()=>{
+        settingsbtn.style.color = "grey"
+    });
+    settingsbtn.addEventListener('mouseout', ()=>{
+        settingsbtn.style.color = "white"
+    });
+    //opening up the settings menu
+    settingsbtn.addEventListener('click', ()=>{
+        formContainer.setAttribute('id','formContainer')
+        settingContainer.setAttribute('id','settingContainer')
+        timerparentContainer.setAttribute("id", "formHidden")
+        //add event listener to submit settings form
+
+        //want to add event listeners after the form is rendered
+        document.addEventListener("DOMContentLoaded",()=>{
+            //event listener for exiting the settings form
+            settingCancel.addEventListener('click', ()=>{
+                formContainer.setAttribute('id','formHidden')
+                settingContainer.setAttribute('id','formHidden')
+                timerparentContainer.setAttribute('id', "timerParentContainer")
+            });
+        });
+    });
+
+//Event listeners for login
+    //mouse over event to change text color
+    loginbtn.addEventListener('mouseover', ()=>{
+        loginbtn.style.color = "grey"
+    });
+    loginbtn.addEventListener('mouseout', ()=>{
+        loginbtn.style.color = "white"
+    });
+
+    //opening up the login form
+    loginbtn.addEventListener('click', ()=>{
+        formContainer.setAttribute('id','formContainer')
+        loginContainer.setAttribute('id','loginContainer')
+        timerparentContainer.setAttribute("id", "formHidden")
+        //add event listener to submit log in form
+
+        //want to access log in cancel after its loaded
+        document.addEventListener("DOMContentLoaded", ()=>{
+            //event listener to exit log in form
+            loginCancel.addEventListener('click',()=>{
+                formContainer.setAttribute('id','formHidden')
+                loginContainer.setAttribute('id','formHidden')
+                timerparentContainer.setAttribute('id', "timerParentContainer")
+            });
+        });
+    });
+
+//Event Listeners for sign up
+    //mouseover event to change text color
+    signupbtn.addEventListener("mouseover",()=>{
+        signupbtn.style.color = "grey"
+    })
+    signupbtn.addEventListener('mouseout', ()=>{
+        signupbtn.style.color = "white"
+    });
+
+    //opening up sign up form
+    signupbtn.addEventListener('click', ()=>{
+        formContainer.setAttribute('id','formContainer')
+        signupContainer.setAttribute('id','signupContainer')
+        timerparentContainer.setAttribute("id", "formHidden")
+        //add event listner to submit sign up form
+            //sumbit-> boolean if pass & confirm pass match => true
+                            //post new user into index.json
+                            //send a new alert "Welcome to myPOMODORO!"
+                            //after 1.5 sec render page without form
+                  //-> boolean if pass & confirm pass no match => false
+                            //send a alert "Password doesn't match"
+                            //reset form 
+
+
+
+        //want to access
+        document.addEventListener("DOMContentLoaded", ()=>{
+            //event listener to exit log in form
+            loginCancel.addEventListener('click',()=>{
+                formContainer.setAttribute('id','formHidden')
+                signupContainer.setAttribute('id','formHidden')
+                timerparentContainer.setAttribute('id', "timerParentContainer")
+            });
+        });
+    })
