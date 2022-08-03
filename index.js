c04ac87e09d43f883ee9dcfd14fd6bd0ef6d12ac
KarlSavinskiy <karlsavinskiy@gmail.com>
2022-08-03 11:28:19 -0400

    log in button function and setting function

--- index.js
+++ index.js
@@ -126,7 +126,7 @@ getAllQuotes()
 
 
 // Karl's code sign up form
-document.querySelector('#signupForm').addEventListener("submit", (e) => {
+document.querySelector('#submitCreate').addEventListener("click", (e) => {
     e.preventDefault()
     createAnAccount()})
 
@@ -136,10 +136,8 @@ async function createAnAccount() {
     const confirmPasswordSubmitForm = document.querySelector('#confirmPasswordSubmitForm');
 
     objNewInfoSingUp = {
-        loginInfo: {
         username: usernameSubmitForm.value,
-        password: passwordSubmitForm.value
-        },
+        password: passwordSubmitForm.value,
         startWorkTimer: 25,
         shortBreakTimer: 5,
         longBreakTimer: 10,
@@ -152,9 +150,11 @@ async function createAnAccount() {
         formContainer.setAttribute('id','formHidden');
         signupContainer.setAttribute('id','formHidden');
         timerparentContainer.setAttribute('id', "timerParentContainer");
-        
+        alert('Successfully sign up! Please log in!')
+
     } else {
         document.querySelector('#signupForm').reset();
+        alert('Your passwords do not match. Please try again.')
     }
 }
 
@@ -167,3 +167,132 @@ async function updateYourPasswordAndName(objNewInfoSingUp) {
     let data = await res.json() ;
     console.log(data);
 }
+
+/* I did the button again, we don't need it; 
+const cancelButtonSignUp = document.querySelector('#cancelSignUpButton')
+cancelButtonSignUp.addEventListener('click',(e) => hideTheForm())
+
+function hideTheForm() {
+    formContainer.setAttribute('id','formHidden');
+    signupContainer.setAttribute('id','formHidden');
+    timerparentContainer.setAttribute('id', "timerParentContainer");
+}
+*/
+//Karl's settings
+
+document.querySelector('#submitSetting').addEventListener("click", (e) => {
+    e.preventDefault()
+    changeSettings()})
+
+async function changeSettings() {
+        const workTimerForm = document.querySelector('#workTimerForm');
+        const shortBreakTimerForm = document.querySelector('#shortBreakTimerForm');
+        const longBreakTimerForm = document.querySelector('#longBreakTimerForm');
+    
+        objYourSettings = {
+            startWorkTimer: workTimerForm.value,
+            shortBreakTimer: shortBreakTimerForm.value,
+            longBreakTimer: longBreakTimerForm.value
+        }
+        document.querySelector('#timerMin').innerText = workTimerForm.value;
+        //shortBreak.innerText = shortBreakTimerForm.value;
+        //longBreak.innerHTML = longBreakTimerForm.value
+        formContainer.setAttribute('id','formHidden');
+        signupContainer.setAttribute('id','formHidden');
+        timerparentContainer.setAttribute('id', "timerParentContainer");
+        alert('Wow! You changed your settings!')
+            /* (////you are loged in; needs to be changed //// workTimerForm.value == "wow") {
+            updateYourSettings(objYourSettings, yourId);
+            alert('Wow! You changed your settings!')
+            } else {
+            document.querySelector('#timerMin').innerText = workTimerForm.value;
+            //shortBreak.innerText = shortBreakTimerForm.value;
+            //longBreak.innerHTML = longBreakTimerForm.value
+            formContainer.setAttribute('id','formHidden');
+            signupContainer.setAttribute('id','formHidden');
+            timerparentContainer.setAttribute('id', "timerParentContainer");
+            alert('Wow! You changed your settings!')
+        }
+        */
+        
+}
+    
+async function updateYourSettings(objYourSettings, yourId) {
+         let res = await fetch(`http://localhost:3000/users/ ${yourId}`, {
+            method: 'PATCH',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(objYourSettings)
+        });
+        let data = await res.json() ;
+        console.log(data);
+}
+    
+//Karl's Log in
+
+
+document.querySelector('#submitLogin').addEventListener("click", (e) => {
+    e.preventDefault()
+    getDataFromOurServer()})
+
+
+async function getDataFromOurServer() {
+    let res = await fetch('http://localhost:3000/users');
+    let dataFromServer = await res.json() ;
+    //let valuesOfNames = Object.keys(dataFromServer);
+    //console.log(valuesOfNames) 
+    const usernameLoginForm = document.querySelector('#usernameLoginForm');
+    const passwordLoginForm = document.querySelector('#passwordLoginForm');
+    let idName = 'None';
+    let IdPasword = 'None';
+    dataFromServer.forEach((oneObj) => {
+        if (oneObj.username == usernameLoginForm.value) {
+            idName = oneObj.id;
+            console.log(idName)
+        } else {console.log('not match')}
+        }
+    )
+    dataFromServer.forEach((oneObj) => {
+        if (oneObj.password == passwordLoginForm.value) {
+            IdPasword = oneObj.id;
+            console.log(IdPasword)
+        } else {console.log('not match')}
+    })
+
+    if(idName == IdPasword && idName != 'None' && IdPasword != 'None') {console.log('you are loged in')
+    async function getdataForUser() {
+        let res = await fetch(`http://localhost:3000/users/${idName}`);
+        let dataForUser = await res.json() ;
+        console.log(dataForUser);
+    }
+    getdataForUser()
+    }
+     else {
+        console.log('your password or name is incorect')
+    }
+
+
+    //console.log( dataFromServer)
+   // checkIfYouAreSignedUp(dataFromServer);
+}
+
+// async function checkIfYouAreSignedUp(dataFromServer) {
+//     const usernameLoginForm = document.querySelector('#usernameLoginForm');
+//     const passwordLoginForm = document.querySelector('#passwordLoginForm');
+//     if(dataFromServer.forEach((oneObj) => {
+//      oneObj.username == usernameLoginForm.value
+        
+//     }) &&  dataFromServer.forEach((oneObj) => {
+//      oneObj.password == passwordLoginForm.value
+//     })) {
+//         console.log('wow')
+//     } else {
+
+//     console.log('not wow')
+//     }
+// }
+   
+
+
+
+
+
